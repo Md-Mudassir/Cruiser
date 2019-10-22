f908f0671857b63da2218d1c59cbec00509a37d4
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-22 23:43:15 +0530

    json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3700;
 
 app.use('/', (req, res) => {
-  res.status(200).send('Hello');
+  res.status(200).json({ message: 'Hello' });
 });
 
 app.listen(PORT, () => {

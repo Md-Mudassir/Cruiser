6cebc948db97a82603d5e17687f953c488270859
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-23 10:38:33 +0530

    json api

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3700;
 
-app.use('/', (req, res) => {
-  res.status(200).json({ message: 'Hello' });
-});
+// app.use('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello' });
+// });
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    data: {
+      tours: tours
+    }
+  });
+});
 app.listen(PORT, () => {
   console.log(`Started @ ${PORT}`);
 });

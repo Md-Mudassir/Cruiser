9697e3a27ed350dd9c1e7a9939e59192540d3418
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-23 12:29:21 +0530

    post request

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ app.use(express.json());
 // app.use('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello' });
 // });
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('done');
-});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -24,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 app.listen(PORT, () => {
   console.log(`Started ${PORT}`);

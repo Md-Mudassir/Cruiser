b508c237fe74eba0b00060d07b17ceee71451e9f
parvezshah24 <51455383+parvezshah24@users.noreply.github.com>
2019-10-29 14:19:39 +1100

    created request and response functions

--- app.js
+++ app.js
@@ -22,3 +22,8 @@ app.get('/api/v1/tours', (req, res) => {
     }
   });
 });
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);

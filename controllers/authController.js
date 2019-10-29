d5f3cc63a09a259467cc730e179e6a4eff0f8807
parvezshah24 <51455383+parvezshah24@users.noreply.github.com>
2019-10-29 14:32:53 +1100

    token validation is done

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,3 +77,22 @@ exports.protect = catchAsync(async (req, res, next) => {
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }

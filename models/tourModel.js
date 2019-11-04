ca46ff3e52e5a24e26d58e7f0a68bb71b830b602
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-04 12:04:07 +0530

    models update

--- models/tourModel.js
+++ models/tourModel.js
@@ -131,23 +131,6 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// QUERY MIDDLEWARE
-tourSchema.pre(/^find/, function(next) {
-  this.find({ secretTour: { $ne: true } });
-
-  this.start = Date.now();
-  next();
-});
-
-tourSchema.pre(/^find/, function(next) {
-  this.populate({
-    path: 'guides',
-    select: '-__v -passwordChangedAt'
-  });
-
-  next();
-});
-
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
